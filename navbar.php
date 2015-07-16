1d0d84456f051802182b1e4c639d2a86398cce4e
Nikolas Rassoules <nikolasrassoules@gmail.com>
2015-07-16 16:06:04 -0400

    added navbar

---
+++ navbar.php
@@ -0,0 +1,9 @@
+<nav id="navbar">
+	<ul class="navbarul">
+		<li class="navbarli leftLi"><a href="">Register</a></li>
+		<li class="navbarli leftLi"><a href="">Trade</a></li>
+		<h1 class="brand">TradeYoKickz</h1>
+		<li class="navbarli rightLi"><a href="">Sell</a></li>
+		<li class="navbarli rightLi"><a href="">Log In</a></li>
+	</ul>
+</nav>
