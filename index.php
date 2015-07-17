f1f5338c79ec988e85c25db7403aec0e66db2793
Nikolas Rassoules <nikolasrassoules@gmail.com>
2015-07-17 11:39:02 -0400

    mobile nav bar and background

--- index.php
+++ index.php
@@ -2,17 +2,23 @@
 <html>
 <head>
 	<title>TradeYoKickz</title>
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
 	<link href='http://fonts.googleapis.com/css?family=Oswald:400,300' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="main.css">
 </head>
 <body id="indexBg">
 	<?php 
 		include "navbar.php";
-	?>
+	?>	
+	<section></section>
+	<div id="mobileBar">
+		<p class="borderP"><i class="fa fa-bars icon-border"></i>TradeYoKickz</p>
+	</div>
 	<div class="hero">
 		<h1 id="centerText">STAY FLY WITH <span class="brand">TYK</span></h1>
 		<h2 id="centerText2">Trade and Sell your used, deadstock, or custom kicks!</h2>
 		<center><a href="register.php"><button class="register">Register</button></a></center>
 	</div>
+
 </body>
 </html>
