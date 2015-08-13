487ea2919963590b2c953b140d84be65997501cb
Nikolas Rassoules <nikolasrassoules@gmail.com>
2015-08-13 16:32:11 -0400

    deletes the extra files

--- loginForm.php
+++
@@ -1,9 +0,0 @@
-<div id="logInBox">
-	<h1 style="margin-top:40px;">Login</h1>
-	<div class="login">
-		<h3 class="topSpace">Username: </h3><input name="username" />
-		<h3 class="topSpace">Password: </h3><input name="password" type="password" />
-		<br>
-		<button type="submit" class="submit" style="height:35px;">Log In</button>	
-	</div>
-</div>
